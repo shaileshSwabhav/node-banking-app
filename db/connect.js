e7f0a80c0990ab83efb31bfcb98454780e671e28
shaileshSwabhav <shailesh@swabhavtechlabs.com>
2023-02-20 13:26:33 +0530

    Connection changes for db

--- db/connect.js
+++ db/connect.js
@@ -3,8 +3,8 @@ const { Sequelize } = require("sequelize")
 
 const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   dialect: process.env.DB_DIALECT,
-  host: process.env.DBHOST,
-  port: process.env.DBPORT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   logging: console.log,
 })
 
